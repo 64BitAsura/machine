0943beb3d73d668b7a84bf13cb7f11e9dcb0a534
logakris <sambath.logakrishnan@nokia.com>
2018-12-12 19:56:59 +0100

    update 1.2.1

--- i3/scripts/docker-stop.sh
+++ i3/scripts/docker-stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Right click
     if [[ "${BLOCK_BUTTON}" -eq 3 ]]; then
-        docker stop $(docker ps -q)
+        $(docker stop $(docker ps -q))
     fi
     count=$(docker ps --format '{{.Names}}' | wc -l)
     echo "<span color=\"#FE1C1C\">  Docker $count</span>"
