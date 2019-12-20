9d8fabba8d10cfd6d12ca3586a3c947303935d3b
sambathl <samkumar15@gmail.com>
2019-12-20 17:07:51 +0100

    v13

--- polybar/ssh.sh
+++ polybar/ssh.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ssh_connections="$(ss | grep "ssh" | wc -l)"
-ssh_connections="$(expr $ssh_connections / 2)"
 zero="0"
 if [ "$ssh_connections" -gt "$zero" ]; then
     echo "%{F#e62}%{F-} $ssh_connections"
